f1a478cc24b89cf1cbf702205c10f0aa1c6b358d
joaofolle <joaofolle@joaofolle-A530-T-BE76P1>
2015-10-26 19:59:19 -0200

    Inserção da validação correta do CPF. Falta arrumar ainda a resposta de usuário adicionado.

--- src/java/br/edu/ifrs/restinga/modelo/Usuarios.java
+++ src/java/br/edu/ifrs/restinga/modelo/Usuarios.java
@@ -72,8 +72,38 @@ public class Usuarios {
     }
     
     public void validarCPF() throws Exception {
-        if (this.cpf.equals("111")) {
-            throw new Exception("CPF inválido");
+        String CPF              = this.cpf;                                                                                       //STRING CPF
+                                                    
+        int primeira_soma = 0, primeiro_peso = 10, numero_percorrido = 0;                                                               //INT primeira_soma e primeiro_peso
+        
+        for(int i = 0; i <= 8; i++){                    
+            numero_percorrido = Integer.parseInt(String.valueOf(CPF.charAt(i)));       
+            primeira_soma += numero_percorrido * primeiro_peso;                
+            primeiro_peso--;                                                   
+        }//Fecha o for
+        
+        int resto_primeira_soma = primeira_soma % 11;                                                                                   //INT resto da primeira soma
+        int pdv = Integer.parseInt(String.valueOf(CPF.charAt(9)));                                                                      //INT primeiro digito verificador
+        
+        if (resto_primeira_soma < 2 && pdv == 0 || 11 - resto_primeira_soma == pdv) {
+            int segunda_soma = 0, segundo_peso = 11;                                                                                    //INT segunda_soma e  segundo_peso
+            
+            for(int i = 0; i <= 9; i++){
+                numero_percorrido = Integer.parseInt(String.valueOf(CPF.charAt(i)));    
+                segunda_soma += numero_percorrido * segundo_peso;               
+                segundo_peso--;                                                 
+            }//Fecha o for
+            
+            int resto_segunda_soma = segunda_soma % 11;                                                                                 //INT resto da segunda soma
+            int sdv = Integer.parseInt(String.valueOf(CPF.charAt(10)));                                                                 //INT segundo dígitoo verificador
+
+            if(resto_segunda_soma <2 && sdv == 0 || 11 - resto_segunda_soma == sdv){
+                throw new Exception("Usuário cadastrado com sucesso!"); //Valida o CPF
+            }else {
+                throw new Exception("CPF inválido"); //Retorna a falha da segunda verificação;                   
+            }
+        }else {                
+            throw new Exception("CPF inválido");//Retorna a falha da primeira verificação;
         }
     }
     
